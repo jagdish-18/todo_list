1259157e4be7939dc884bf51bf6579fe6a8f390f
jagdish-18 <shiyaljagdish18@gmail.com>
2024-12-14 10:47:46 +0530

    views page changes

--- models/user.model.js
+++ models/user.model.js
@@ -1,35 +1,40 @@
 // const { string } = require("joi");
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-   firstName:{
-        type : String,
-        reqired:true
-   },
-   lastName : String,
-   mobileNo:{
-       type:String,
-   },
-   email:{ 
-       type :String,
-       reqired:true
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      reqired: true,
     },
-    age : {
-        type : String
+    lastName: {
+      type: String,
     },
-    department : {
-        type : String
+    mobileNo: {
+      type: String,
     },
-    password:{
-          type:String    
+    email: {
+      type: String,
+      reqired: true,
     },
-   isDelete:{
-       type:Boolean,
-       default:false
-   }
-},{
-    versionkey:false,
-    timestamp:true
-})
+    age: {
+      type: String,
+    },
+    department: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    isDelete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionkey: false,
+    timestamp: true,
+  }
+);
 
-module.exports = mongoose.model("users",userSchema)
+module.exports = mongoose.model("users", userSchema);
